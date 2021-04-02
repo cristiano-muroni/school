ce682ae8e73667cf6ad00dcffcbe639053c022e4
Cristiano da Silva Muroni <cristiano.muroni@CTNDELLFDHX783.pampa.compasso>
2021-04-02 15:17:24 -0300

    decoded id

--- midlewares/auth.js
+++ midlewares/auth.js
@@ -21,6 +21,7 @@ module.exports = (req, res, next) => {
     }
 
     jwt.verify(token, authConfig.secret, (err, decoded) => {
+      req.userId = decoded.id;          //alteração
       if (err) return res.status(401).send({ error: "Token invalid" });
     });
 
